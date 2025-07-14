1b3a335ba9c0f2ca28255fc25dcfb6ee42924505
bharatkn786 <158079840+bharatkn786@users.noreply.github.com>
2025-07-14 17:09:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-uvicorn main:app --host 0.0.0.0 --port 8000
+# uvicorn main:app --host 0.0.0.0 --port 8000
+
+echo "🔁 Installing dependencies..."
+pip install --upgrade pip
+pip install -r requirements.txt
+
+echo "🚀 Starting app with Gunicorn + Uvicorn worker"
+exec gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app
