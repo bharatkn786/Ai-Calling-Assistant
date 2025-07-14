66367e83b85bca58e2e3df38ccbd73de18856e71
bharatkn786 <158079840+bharatkn786@users.noreply.github.com>
2025-07-14 15:26:47 +0530

    Update call_user.py

--- call_user.py
+++ call_user.py
@@ -14,9 +14,9 @@ print(target_number)
 client = Client(account_sid, auth_token)
 
 # Your actual public Serveo URL with /voice endpoint
-SERVEO_URL="ai-call-assistance-ebh2h7bwgwbmcdh8.centralindia-01.azurewebsites.net"
-# public_url = os.getenv("SERVEO_URL") + "/voice"
-public_url = f"{SERVEO_URL}/voice"
+# SERVEO_URL="ai-call-assistance-ebh2h7bwgwbmcdh8.centralindia-01.azurewebsites.net"
+public_url = os.getenv("SERVEO_URL") + "/voice"
+# public_url = f"{SERVEO_URL}/voice"
 
 print("📞 Voice URL being used in call_user:", public_url)
 call = client.calls.create(
