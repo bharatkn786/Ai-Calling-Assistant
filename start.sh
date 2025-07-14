4f3b86af69c0ca8db44f97b8f40c29c82fa85ff2
bharatkn786 <158079840+bharatkn786@users.noreply.github.com>
2025-07-14 17:36:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # uvicorn main:app --host 0.0.0.0 --port 8000
 
-# echo "🔁 Installing dependencies..."
+echo "🔁 Installing dependencies..."
 pip install --upgrade pip
 pip install -r requirements.txt
 
